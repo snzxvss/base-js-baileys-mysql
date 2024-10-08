5bd22e90735d503cb3f0c0f51e29ec858d28daae
JessiPolo20 <jessicapolo057@gmail.com>
2024-10-08 16:45:45 -0500

    pass mysql

--- src/app.js
+++ src/app.js
@@ -183,7 +183,7 @@ const main = async () => {
         host: "localhost",
         user: "root",
         database: "builderbot",
-        password: "",
+        password: "sanzvoss",
     })
 
     const { handleCtx, httpServer } = await createBot({
